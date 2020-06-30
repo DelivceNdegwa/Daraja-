6d46ae3b57c3fe3e91877578db7b975d2b0fd1a3
DelivceNdegwa <delivce.ndegwa18@gmail.com>
2020-06-30 12:37:24 +0300

    changes

--- Daraja/mpesa_integration/lipa_na_m_pesa.py
+++ Daraja/mpesa_integration/lipa_na_m_pesa.py
@@ -6,10 +6,10 @@ from requests.auth import HTTPBasicAuth
 
 # Key variables 
 lipa_na_m_pesa_shortcode = "174379"
-lipa_na_m_pesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c91"
+lipa_na_m_pesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
 phone_number = "254711994966"
-consumer_key = "aGd3WKTtLpL9AsvGkfi6MaLNqE5H1mCA"
-consumer_secret = "JBqhnOTuxftnTe2E"
+consumer_key = "IvFkneOfx6S6sIgRHrqMtfJVNOGsF9n9"
+consumer_secret = "19htvGwAXUXMAvGU"
 
 # Getting timezone
 raw_timestamp = datetime.now()
@@ -27,10 +27,11 @@ api_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_c
 r = requests.get(api_url, auth=HTTPBasicAuth(consumer_key, consumer_secret))
 json_response = r.json()
 my_access_token = json_response['access_token']
+print(my_access_token)
 
 # Lipa na mpesa transaction
 def lipa_na_mpesa():
-    access_token = ""
+    access_token = my_access_token
     api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
     headers = { "Authorization": "Bearer %s" % access_token }
     request = {
@@ -51,4 +52,4 @@ def lipa_na_mpesa():
   
     print(response.text)
 
-lipa_na_mpesa()        
+#lipa_na_mpesa()        
